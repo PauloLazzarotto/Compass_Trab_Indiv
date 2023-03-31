da800121e2a15dcba2cef95c2394b261701ac624
PauloLazzarotto <root@ip-172-31-11-117.ec2.internal>
2023-03-31 14:08:48 +0000

    Criada validação de Apache (online/offline)

--- scriptValidaApache.sh
+++ scriptValidaApache.sh
@@ -2,4 +2,12 @@
 
 dataEHora=$(date +"%Y-%m-%d %H:%M:%S")
 
-echo $dataEHora
+result=$(systemctl is-active httpd.service)
+
+if [ $result == "active" ]; then
+  status=$"online"
+  mensagem=$"O Apache está de pé e funcionando."
+else
+  status=$"offline"
+  mensagem=$"O Apache está offline."
+fi
